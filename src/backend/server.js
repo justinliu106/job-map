d8926fa90970426c58ce23afa06bd7134cb81a69
justinliu106 <95105031+justinliu106@users.noreply.github.com>
2026-09-21 00:03:05 -0600

    added getCoords for getting coordinates from job address

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,15 +10,36 @@ app.get('/api/jobs', async (req, res) => {
     res.json(result.rows)
 })
 
+async function getCoords(location) {
+    try {
+        const encodedLocation = encodeURIComponent(location);
+        const res = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodedLocation}`);
+        const data = await res.json();
+
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        const coord = [];
+        coord.push(lat, lon);
+        return coord
+    } catch (error) {
+        console.log("Getcoords failed.", error)
+    }
+}
+
 app.post('/api/jobs', async (req, res) => {
     const { id, title, company, exactLocation, location, salary, url } = req.body;
+    const coords = await getCoords(exactLocation);
+    const lat = coords[0];
+    const lon = coords[1];
 
     await pool.query(
-        `INSERT INTO jobs (id, title, company, exact_location, location, salary, url) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        `INSERT INTO jobs (id, title, company, exact_location, location, salary, url, latitude, longitude) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
         ON CONFLICT (id) DO UPDATE SET 
-        exact_location = COALESCE(EXCLUDED.exact_location, jobs.exact_location)`,
-        [id, title, company, exactLocation, location, salary, url]
+        exact_location = COALESCE(EXCLUDED.exact_location, jobs.exact_location),
+        latitude = COALESCE(EXCLUDED.latitude, jobs.latitude),
+        longitude = COALESCE(EXCLUDED.longitude, jobs.longitude)`,
+        [id, title, company, exactLocation, location, salary, url, lat, lon]
     );
     res.sendStatus(200);
 })
